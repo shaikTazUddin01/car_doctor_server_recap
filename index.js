5c8928bcefc0145a29d9935811c91765120c2643
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-01 16:19:34 +0600

    get specific service

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,20 @@ const database = client.db("carDoctor");
 const services = database.collection("services");
 
 app.get('/services',async(req,res)=>{
-
     const result=await services.find().toArray()
-
     res.send(result)
 })
-
+// get specific service data
+app.get('/services/:id',async(req,res)=>{
+  const id=req.params.id
+  const query={_id : new ObjectId(id)}
+  const options = {
+    
+    projection: { _id: 1, title: 1, img: 1,price :1 , description :1 },
+  };
+  const result =await services.findOne(query,options)
+  res.send(result)
+})
 
 
 async function run() {
